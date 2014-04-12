e7a06344df521adf9c138eea6600b6d4b3d5bf8e
Edisonthk <edisonthk@gmail.com>
2014-04-12 22:36:24 +0900

    2

---
+++ TestForClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestForClass
+//
+//  Created by Edisonthk on 4/12/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(IBAction)sayHello;
+
+@end
