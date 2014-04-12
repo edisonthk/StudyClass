954525a4c22c4b4954b3c8510a5f76e3e0bd027e
Edisonthk <edisonthk@gmail.com>
2014-04-12 19:47:11 +0900

    Initial Commit

---
+++ TestForClass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestForClass
+//
+//  Created by Edisonthk on 4/12/14.
+//  Copyright (c) 2014 Edisonthk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
